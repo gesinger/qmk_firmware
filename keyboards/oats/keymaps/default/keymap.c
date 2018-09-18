e61f9c95f4020641bd83c749414bbf8d9cd8a2a4
Garrett Singer <gesinger@gmail.com>
2018-09-17 19:16:58 -0700

    Start adding layers

--- keyboards/oats/keymaps/default/keymap.c
+++ keyboards/oats/keymaps/default/keymap.c
@@ -7,9 +7,13 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _TOP 0
+#define _NUMS 1
+#define _SPECIAL 2
 
 enum custom_keycodes {
   TOP = SAFE_RANGE,
+  NUMS,
+  SPECIAL,
 };
 
 // Fillers to make layering more clear
@@ -19,10 +23,34 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_TOP] = LAYOUT( \
-  KC_Q, KC_W, KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I, KC_O, KC_P, \
-  KC_A, KC_S, KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K, KC_L, KC_SCOLON, \
-  KC_Z, KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMMA, KC_DOT, KC_QUESTION, \
-  DEBUG, KC_SPACE, KC_LSHIFT,     KC_BSPACE, KC_ENTER, KC_TAB \
+  KC_Q, KC_W, KC_E, KC_R, KC_T, \
+  KC_Y, KC_U, KC_I, KC_O, KC_P, \
+
+  LT(NUMS, KC_A), CTL_T(KC_S),  ALT_T(KC_D), LCMD_T(KC_F), SFT_T(KC_G), \
+  SFT_T(KC_H),    RCMD_T(KC_J), ALT_T(KC_K), CTL_T(KC_L),  LT(NUMS, KC_SCLN), \
+
+  KC_Z, KC_X, KC_C,    KC_V,   KC_B, \
+  KC_N, KC_M, KC_COMM, KC_DOT, KC_QUES, \
+
+  TG(NUMS), KC_LSFT, KC_BSPC, \
+  KC_ENT,   KC_SPC,  KC_TAB \
+),
+
+[_NUMS] = LAYOUT( \
+  KC_SLSH, KC_7, KC_8, KC_9, KC_MINS,   KC_EXLM, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV, \
+
+  LT(SPECIAL, KC_ASTR), CTL_T(KC_4),     ALT_T(KC_5),    LCMD_T(KC_6),   SFT_T(KC_PLUS), \
+  SFT_T(KC_EQL),        RCMD_T(KC_QUOT), ALT_T(KC_LPRN), CTL_T(KC_RPRN), LT(SPECIAL, KC_AMPR), \
+
+  KC_0, KC_1, KC_2, KC_3, KC_EQL,    KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, \
+  TG(SPECIAL), TG(NUMS), KC_BSPC,    KC_ENT, KC_SPC, KC_TAB \
+),
+
+[_SPECIAL] = LAYOUT( \
+  KC_ACL2, KC_ESC,    BL_DEC,   BL_INC,   KC_MS_BTN1,   XXXXXXX, LSFT(LALT(KC__VOLDOWN)), LSFT(LALT(KC__VOLUP)), XXXXXXX, KC__VOLUP, \
+  KC_ACL1, KC_MS_L,   KC_MS_D,  KC_MS_U,  KC_MS_R,      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC__VOLDOWN, \
+  KC_ACL0, KC_LCTL,   MOD_LALT, KC_RCMD,  KC_MS_BTN2,   KC_HOME, KC_PGDN, KC_UP, KC_END, KC__MUTE, \
+  KC_ESC,  TG(SPECIAL), KC_BSPC,                        KC_ENT, KC_SPC, KC_TAB \
 ),
 
 };
--- keyboards/oats/rules.mk
+++ keyboards/oats/rules.mk
@@ -48,7 +48,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no        # Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
@@ -58,7 +58,7 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
-SLEEP_LED_ENABLE = no    	 # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 USE_I2C = yes
 SPLIT_KEYBOARD = yes
