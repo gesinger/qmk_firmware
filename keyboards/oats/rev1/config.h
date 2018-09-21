4eda537cefa0b18d20dbc25231f7d159405d18b3
Garrett Singer <gesinger@gmail.com>
2018-09-20 20:18:22 -0700

    Improve mousekey settings for oats keyboard and make a few more adjustments to default keymap.

--- keyboards/oats/rev1/config.h
+++ keyboards/oats/rev1/config.h
@@ -32,4 +32,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 500
 
+// from ergodox_ez
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
+
 #endif
