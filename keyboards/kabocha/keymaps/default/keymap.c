c94b9a03ef6c8573acf0c760c688093fc82c6e69
Garrett Singer <gesinger@gmail.com>
2018-12-30 05:01:37 -0500

    Change kabocha keyoard from Colemak mod dh to Colemak mod dhm.

--- keyboards/kabocha/keymaps/default/keymap.c
+++ keyboards/kabocha/keymaps/default/keymap.c
@@ -20,12 +20,12 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  // colemak mod dh
+  // colemak mod dhm (mod dh with k and m switched for ortholinear keyboards)
   [_TOP] = LAYOUT(
     KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, XXXXX,
-    KC_A, KC_R, KC_S, KC_T, KC_G, KC_K, KC_N, KC_E, KC_I, KC_O,
+    KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O,
     // TODO https://github.com/qmk/qmk_firmware/pull/2900 for ;/, ./: in place of </>
-    KC_Z, KC_X, KC_C, KC_D, KC_V, KC_M, KC_H, KC_COMM, KC_DOT, KC_SLSH,
+    KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
     TT(_COMMANDS), KC_LALT, KC_LCTL, TT(_SPECIAL), SFT_T(KC_BSPC), LCMD_T(KC_SPC), TT(_NUMS), KC_RCTL, XXXXX, XXXXX
   ),
 
