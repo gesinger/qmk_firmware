bc2cb7a69a3029c2a3cf16954e4ad68447a30eb0
Garrett Singer <gesinger@gmail.com>
2019-01-13 16:23:29 -0500

    Change oats keyboard layout to use colemak mod dhm

--- keyboards/oats/keymaps/default/keymap.c
+++ keyboards/oats/keymaps/default/keymap.c
@@ -9,49 +9,49 @@ extern keymap_config_t keymap_config;
 #define _TOP 0
 #define _NUMS 1
 #define _SPECIAL 2
+#define _COMMANDS 3
 
 #define FINEVOLUP LSFT(LALT(KC__VOLUP))
 #define FINEVOLDN LSFT(LALT(KC__VOLDOWN))
-#define TABR LCMD(KC_TAB)
-#define TABL LSFT(LCMD(LSFT(KC_TAB)))
-
-enum custom_keycodes {
-  TOP = SAFE_RANGE,
-  NUMS,
-  SPECIAL,
-};
+#define APPR LCMD(KC_TAB)
+#define APPL LCMD(LSFT(KC_TAB))
+#define TABR LCTL(KC_TAB)
+#define TABL LCTL(LSFT(KC_TAB))
 
 // Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#define _____ KC_TRNS
+#define XXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_TOP] = LAYOUT( \
-  KC_Q, KC_W, KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,   KC_P, \
+  [_TOP] = LAYOUT(
+    KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, XXXXX,
+    KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O,
+    // TODO https://github.com/qmk/qmk_firmware/pull/2900 for ;/, ./: in place of </>
+    KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
+    TT(_SPECIAL), SFT_T(KC_BSPC), TT(_COMMANDS), KC_RCTL, LCMD_T(KC_SPC), TT(_NUMS)
+  ),
 
-  CTL_T(KC_A), LCMD_T(KC_S), LT(_NUMS, KC_D), LT(_SPECIAL, KC_F), LALT_T(KC_G), \
-    RALT_T(KC_H), LT(_SPECIAL, KC_J), LT(_NUMS, KC_K), RCMD_T(KC_L), CTL_T(KC_SCLN), \
+  [_NUMS] = LAYOUT(
+    XXXXX, KC_7, KC_8, KC_9, KC_BSLS, KC_ESC, KC_PIPE, KC_LBRC, KC_RBRC, XXXXX,
+    KC_0, KC_4, KC_5, KC_6, KC_PLUS, KC_ENT, KC_QUOT, KC_LPRN, KC_RPRN, KC_MINS,
+    // TODO KC_LT, KC_GT swap with shifted first layer , and .
+    KC_GRV, KC_1, KC_2, KC_3, KC_EQL, KC_SCLN, KC_TAB, KC_LCBR, KC_RCBR, KC_COLN,
+    _____, _____, _____, _____, _____, _____
+  ),
 
-  SFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT, SFT_T(KC_SLSH), \
+  [_SPECIAL] = LAYOUT(
+    KC_DEL, XXXXX, XXXXX, XXXXX, XXXXX, KC__MUTE, FINEVOLDN, FINEVOLUP, KC__VOLDOWN, KC__VOLUP,
+    XXXXX, XXXXX, APPL, APPR, XXXXX, KC_BRIU, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
+    XXXXX, XXXXX, TABL, TABR, XXXXX, KC_BRID, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    _____, _____, _____, _____, _____, _____
+  ),
 
-  KC_TAB, SFT_T(KC_BSPC), XXXXXXX,   XXXXXXX, SFT_T(KC_SPC), KC_ENT \
-),
-
-[_NUMS] = LAYOUT( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,   KC_7,    KC_8,    KC_9,    KC_0, \
-  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_MINS,   KC_GRV, KC_DQUO, KC_QUOT, KC_LCBR, KC_RCBR, \
-  KC_ASTR, KC_EXLM, KC_EQL,  KC_PLUS, KC_UNDS,   KC_AT,  KC_HASH, KC_DLR,  KC_PERC, KC_BSLS, \
-
-  _______, _______, _______,            _______, _______, _______ \
-),
-
-[_SPECIAL] = LAYOUT( \
-  KC_ESC,  XXXXXXX, BL_DEC,  BL_INC,  KC_MS_BTN1,   TABL,    TABR,    FINEVOLDN, FINEVOLUP, KC__VOLUP, \
-  XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,      KC_LEFT, KC_DOWN, KC_UP,     KC_RIGHT,  KC__VOLDOWN, \
-  XXXXXXX, XXXXXXX, KC_TAB,  KC_RCMD, KC_MS_BTN2,   KC_HOME, KC_PGDN, KC_PGUP,   KC_END,    KC__MUTE, \
-
-  _______, _______, _______,                         _______, _______, _______ \
-),
+  [_COMMANDS] = LAYOUT(
+    XXXXX, XXXXX, XXXXX, XXXXX, RESET, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,
+    XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,
+    XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,
+    _____, _____, _____, _____, _____, _____
+  ),
 
 };
