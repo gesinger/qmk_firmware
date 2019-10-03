23adab49e4ba18b0e70762d120a64c08c49f38c1
Garrett Singer <gesinger@gmail.com>
2019-10-03 07:10:19 -0400

    Update dragonwell default keymap

--- keyboards/dragonwell/keymaps/default/keymap.c
+++ keyboards/dragonwell/keymaps/default/keymap.c
@@ -8,30 +8,33 @@
 #define _____ KC_TRNS
 #define XXXXX KC_NO
 
+#define FINEVOLUP LSFT(LALT(KC__VOLUP))
+#define FINEVOLDN LSFT(LALT(KC__VOLDOWN))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_TOP] = LAYOUT(
     ALT_T(KC_V), KC_W, KC_F, KC_P, KC_L, KC_U, KC_Y, ALT_T(KC_K),
     LT(_MOUSE, KC_A), LT(_SYMBOLS, KC_R), CTL_T(KC_S), CMD_T(KC_T), CMD_T(KC_N), CTL_T(KC_E), LT(_SYMBOLS, KC_I), LT(_MOUSE, KC_O),
-    KC_B, KC_D, KC_C, SFT_T(KC_BSPC), LT(_NUMS, KC_SPC), KC_M, KC_H, KC_G
+    KC_B, KC_D, KC_C, SFT_T(KC_BSPC), LT(_NUMS, KC_SPC), KC_H, KC_M, KC_G
   ),
 
   [_NUMS] = LAYOUT(
     ALT_T(KC_7), KC_8, KC_9, KC_EQL, KC_Q, KC_J, KC_X, ALT_T(KC_Z),
     KC_4, KC_5, CTL_T(KC_6), CMD_T(KC_0), CMD_T(KC_ENT), CTL_T(KC_TAB), KC_LEAD, KC_ESC,
-    KC_1, KC_2, KC_3, _____, _____, _____, _____, _____
+    KC_1, KC_2, KC_3, _____, _____, KC_COMM, KC_DOT, KC_QUES
   ),
 
   [_SYMBOLS] = LAYOUT(
-    ALT_T(KC_BSLS), KC_LCBR, KC_RCBR, KC_ASTR, KC_PLUS, KC_SCLN, KC_COLN, ALT_T(KC_AMPR),
-    KC_SLSH, KC_LPRN, CTL_T(KC_RPRN), CMD_T(KC_UNDS), CMD_T(KC_MINS), CTL_T(KC_COMM), KC_DOT, KC_QUES,
-    KC_PIPE, KC_LBRC, KC_RBRC, _____, _____, KC_DQUO, KC_QUOT, KC_EXLM
+    ALT_T(KC_BSLS), KC_LCBR, KC_RCBR, KC_ASTR, KC_PLUS, KC_AMPR, KC_PIPE, ALT_T(KC_GRV),
+    KC_SLSH, KC_LPRN, CTL_T(KC_RPRN), CMD_T(KC_UNDS), CMD_T(KC_MINS), CTL_T(KC_DQT), KC_QUOT, KC_EXLM,
+    KC_AT, KC_LBRC, KC_RBRC, _____, _____, KC_SCLN, KC_COLN, KC_TILD
   ),
 
   [_MOUSE] = LAYOUT(
-    _____, _____, _____, KC_BTN1, KC_BTN2, _____, _____, _____,
+    KC_CIRC, KC_HASH, KC_DLR, KC_BTN1, KC_BTN2, KC_PGUP, FINEVOLUP, RESET,
     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
-    _____, _____, _____, _____, _____, _____, _____, _____
+    KC_LT, KC_GT, KC_PERC, _____, _____, KC_PGDN, FINEVOLDN, KC__MUTE
   ),
 
 };
