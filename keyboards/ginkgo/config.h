a74e94b90f70d40c0a1d6da315cc1fd155a29612
Garrett Singer <gesinger@gmail.com>
2019-10-17 05:42:00 -0400

    Update ginkgo default keymap, remove inner thumb key mappings

--- keyboards/ginkgo/config.h
+++ keyboards/ginkgo/config.h
@@ -43,4 +43,11 @@
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
+/* from ergodox_ez mouse settings */
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
+
 #endif
